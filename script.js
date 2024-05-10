d3388e35772ec6e14586bfe276dea1ab82644960
Berkres <168309305+Berkres@users.noreply.github.com>
2024-05-10 17:00:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ hamburger.addEventListener("click", () => {
   lexendDecaFont.classList.toggle("active");
   centerHeader.classList.toggle("active");
 })
+
+function SendMail() {
+  var params = {
+      from_name : document.getElementById("name").value,
+      email_id : document.getElementById("email_id").value,
+      message : document.getElementsById("message").value
+  };
+
+  emailjs.send("service_tlz8uzl", "template_vrttl0i", params).then(alert("Message was sent succesfully!"))
+}
+
+function abc(){
+  alert("HAHAHAH")
+}
